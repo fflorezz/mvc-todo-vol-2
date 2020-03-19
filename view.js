c7bc34055f589a59c2f583ce22f67e9e0d30711d
fflorezz <felipeflorezart@gmail.com>
2020-03-18 20:05:23 -0500

    Added modules, helpers and templates

--- view.js
+++ view.js
@@ -1,3 +1,15 @@
+import { qs, $on, $delegate } from "./helpers.js";
+
+const _itemId = element =>
+  parseInt(
+    element.parentNode.dataset.id || element.parentNode.parentNode.dataset.id,
+    10
+  );
+const ENTER_KEY = 13;
+const ESCAPE_KEY = 27;
+
 export default class View {
-  constructor() {}
+  //  @param { !Template } template A Template instance
+
+  constructor(template) {}
 }
