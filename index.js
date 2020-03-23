d7c4dd89e0afb729e334fe2a97b2a9e23f8835ec
fflorezz <felipeflorezart@gmail.com>
2020-03-23 16:30:39 -0500

    Set view methods

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
-import Model from "./model.js";
-import View from "/view.js";
 import Controller from "./controller.js";
+import { $on } from "./helpers.js";
+import Template from "./template.js";
+import Model from "./model.js";
+import View from "./view.js";
 
-const app = new Controller(new Model(), new View());
+const app = new Controller(
+  new Model("todos-vanilla-js"),
+  new View(new Template())
+);
